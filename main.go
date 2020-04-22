138abd60e7004ba7717ba63c99fc3cfe57f06812
Adrian Chifor <me@adrianchifor.com>
2020-04-23 00:55:37 +0100

    Update flags

--- main.go
+++ main.go
@@ -10,17 +10,25 @@ import (
 )
 
 var port int
+var versionFlag bool
+var version string = "0.1"
 
 func init() {
-	flag.StringVar(&storage.Project, "project", "", "GCP Project")
-	flag.StringVar(&storage.Bucket, "bucket", "", "GCS Bucket")
+	flag.StringVar(&storage.Project, "project", "", "GCP Project (required)")
+	flag.StringVar(&storage.Bucket, "bucket", "", "GCS Bucket (required)")
 	flag.IntVar(&port, "port", 8080, "Server port")
+	flag.BoolVar(&versionFlag, "version", false, "Version")
 	flag.Parse()
 }
 
 func main() {
+	if versionFlag {
+		fmt.Println("Bigbucket version", version)
+		os.Exit(0)
+	}
+
 	if storage.Project == "" || storage.Bucket == "" {
-		fmt.Println("--project and --bucket flags are required")
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
