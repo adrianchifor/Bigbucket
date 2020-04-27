8fadd66995815b20093f48c471af96b805c1f883
Adrian Chifor <me@adrianchifor.com>
2020-04-27 17:48:12 +0100

    Refactor table ops

--- api/table.go
+++ api/table.go
@@ -3,69 +3,79 @@ package api
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"bigbucket/store"
 	"github.com/gin-gonic/gin"
 )
 
 func listTables(c *gin.Context) {
-	c.JSON(200, gin.H{"tables": getState("bigbucket/.tables")})
-}
-
-func createTable(c *gin.Context) {
-	tableName := c.Query("tableName")
-	if tableName == "" {
-		c.JSON(400, gin.H{"error": "please provide tableName as a querystring parameter"})
+	tables, err := getTables()
+	if err != nil {
+		log.Print(err)
+		c.JSON(500, gin.H{"error": "Internal server error, check logs"})
 		return
 	}
 
-	tables := getState("bigbucket/.tables")
-	if search(tables, tableName) > -1 {
-		c.JSON(200, gin.H{"success": fmt.Sprintf("%s table already exists", tableName)})
-	} else {
-		tables = append(tables, tableName)
-		err := writeState("bigbucket/.tables", tables)
-		if err != nil {
-			log.Print(err)
-			c.JSON(500, gin.H{"error": "internal server error, check logs"})
-			return
+	// Remove tables marked for deletion from results
+	tablesToDelete := getState("bigbucket/.delete_tables")
+	for _, tableToDelete := range tablesToDelete {
+		index := search(tables, tableToDelete)
+		if index > -1 {
+			tables = removeIndex(tables, index)
 		}
-		err = store.WriteObject(fmt.Sprintf("bigbucket/%s/", tableName), []byte(""))
-		if err != nil {
-			log.Print(err)
-			c.JSON(500, gin.H{"error": "internal server error, check logs"})
-			return
-		}
-		c.JSON(200, gin.H{"success": fmt.Sprintf("%s table created", tableName)})
 	}
+
+	c.JSON(200, gin.H{"tables": tables})
 }
 
 func deleteTable(c *gin.Context) {
 	tableName := c.Query("tableName")
 	if tableName == "" {
-		c.JSON(400, gin.H{"error": "please provide tableName as a querystring parameter"})
+		c.JSON(400, gin.H{"error": "Please provide tableName as a querystring parameter"})
 		return
 	}
 
-	tables := getState("bigbucket/.tables")
-	index := search(tables, tableName)
-	if index == -1 {
-		c.JSON(200, gin.H{"success": fmt.Sprintf("%s table doesn't exist", tableName)})
+	tablesToDelete := getState("bigbucket/.delete_tables")
+	if search(tablesToDelete, tableName) > -1 {
+		c.JSON(200, gin.H{"success": fmt.Sprintf("'%s' table already marked for deletion", tableName)})
 	} else {
-		tables = removeIndex(tables, index)
-		err := writeState("bigbucket/.tables", tables)
+		tables, err := getTables()
 		if err != nil {
 			log.Print(err)
-			c.JSON(500, gin.H{"error": "internal server error, check logs"})
+			c.JSON(500, gin.H{"error": "Internal server error, check logs"})
 			return
 		}
-		// TODO: need to recurse on objects
-		err = store.DeleteObject(fmt.Sprintf("bigbucket/%s/", tableName))
+
+		if search(tables, tableName) == -1 {
+			c.JSON(404, gin.H{"error": fmt.Sprintf("'%s' table not found", tableName)})
+			return
+		}
+
+		tablesToDelete = append(tablesToDelete, tableName)
+		err = writeState("bigbucket/.delete_tables", tablesToDelete)
 		if err != nil {
 			log.Print(err)
-			c.JSON(500, gin.H{"error": "internal server error, check logs"})
+			c.JSON(500, gin.H{"error": "Internal server error, check logs"})
 			return
 		}
-		c.JSON(200, gin.H{"success": fmt.Sprintf("%s table deleted", tableName)})
+		c.JSON(200, gin.H{"success": fmt.Sprintf("'%s' table marked for deletion", tableName)})
 	}
+}
+
+func getTables() ([]string, error) {
+	objects, err := store.ListObjects("bigbucket/", "/")
+	if err != nil {
+		return nil, err
+	}
+
+	var tables []string
+	for _, table := range objects {
+		cleanTable := strings.Replace(strings.Replace(table, "bigbucket", "", 1), "/", "", -1)
+		if cleanTable != "" {
+			tables = append(tables, cleanTable)
+		}
+	}
+
+	return tables, nil
 }
