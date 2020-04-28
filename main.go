4e836c5dcbcb3502bd672e83fc63bf35e30af266
Adrian Chifor <me@adrianchifor.com>
2020-04-28 17:56:26 +0100

    Change bucket flag

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"bigbucket/api"
 	"bigbucket/store"
@@ -15,7 +16,7 @@ var versionFlag bool
 const version string = "0.1.0"
 
 func init() {
-	flag.StringVar(&store.BucketName, "bucket", "", "GCS Bucket name (required)")
+	flag.StringVar(&store.BucketName, "bucket", "", "Bucket name (required, e.g. gs://<bucket-name>)")
 	flag.IntVar(&port, "port", 8080, "Server port")
 	flag.BoolVar(&versionFlag, "version", false, "Version")
 	flag.Parse()
@@ -32,7 +33,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	store.InitGoog()
+	if strings.HasPrefix(store.BucketName, "gs://") {
+		store.BucketName = strings.Replace(store.BucketName, "gs://", "", 1)
+		store.InitGoog()
+	} else if strings.HasPrefix(store.BucketName, "s3://") {
+		// TODO: Implement S3 backend
+		fmt.Println("S3 backend is not yet implemented")
+		os.Exit(1)
+	} else {
+		fmt.Println("--bucket flag supports Google Cloud Storage as 'gs://<bucket-name>'")
+		os.Exit(1)
+	}
 
 	api.RunServer(port)
 }
